074f74ad9348b30729fdbdbd06f4b3f245279296
Hamza Merzić <hamzamerzic@gmail.com>
2015-08-15 20:45:24 +0200

    Update README.md.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # rpi-public-ip
-Fetching public IP and sending it via email. Useful for creating webserver based on Raspberry Pi.
+The purpose of this tool is developing a daemon for fetching Raspberry Pi's (or any other linux distro) public IP and sending it to a desired email address. After you finish setting up this script, you will informed at a defined timestep if the IP address of your machine has changed.
+
+This script is useful for creating a webserver, VNC or being able to SSH to a dynamic IP host. Knowing the public IP of a dynamic IP host has other useful uses.
+
+
+To install the tool, download the "public_ip_hander.py" file and place it in suitable folder, e.g. "/home/user/". Then edit the fields to, gmail_user and gmail_password.
+
+After that do:
+sudo chmod +x /home/user/public_ip_hander.py
+
+To execute the script write:
+python /home/user/public_ip_handler.py
+
+Now, to setup the script being initialized at a desired timestep, we shall use the builtin cron tool.
+
+To open cron editor, write:
+crontab -e
+If you want to execute the script each day at 12:00, append to the opened editor:
+0 12 * * * python /home/user/public_ip_handler.py
---
+++ public_ip_handler.py
@@ -0,0 +1,51 @@
+import sys
+import subprocess
+import smtplib
+from email.mime.text import MIMEText
+import datetime
+
+# Change to your own account information
+# Account Information
+to = 'your.email@gmail.com' # Email to send to.
+gmail_user = 'senders.email@gmail.com' # Email to send from. (MUST BE GMAIL)
+gmail_password = 'senderspassword' # Gmail password.
+smtpserver = smtplib.SMTP('smtp.gmail.com', 587) # Server to use.
+
+smtpserver.ehlo()  # Says 'hello' to the server
+smtpserver.starttls()  # Start TLS encryption
+smtpserver.ehlo()
+smtpserver.login(gmail_user, gmail_password)  # Log in to server
+today = datetime.date.today()  # Get current time/date
+
+arg='touch ~/stored_ip; cat ~/stored_ip'
+# Runs 'arg' in a 'hidden terminal'.
+p=subprocess.Popen(arg,shell=True,stdout=subprocess.PIPE)
+data = p.communicate()  # Get data from 'p terminal'.
+
+# Store previous ip.
+previous_ip = str(data[0])
+
+arg="curl -s checkip.dyndns.org|sed -e 's/.*Current IP Address: //' -e 's/<.*$//\'"  # Linux command to retrieve public ip address.
+p=subprocess.Popen(arg,shell=True,stdout=subprocess.PIPE)
+data = p.communicate() 
+
+# Store current ip.
+current_ip = str(data[0])
+
+if previous_ip == current_ip:
+	smtpserver.quit()
+	exit()
+
+# Creates the text, subject, 'from', and 'to' of the message.
+msg = MIMEText("Your public ip is " + current_ip[:-1] + ".")
+msg['Subject'] = "RaspberryPi's public IP on %s" % today.strftime('%b %d %Y')
+msg['From'] = gmail_user
+msg['To'] = to
+# Sends the message.
+smtpserver.sendmail(gmail_user, [to], msg.as_string())
+# Closes the smtp server.
+smtpserver.quit()
+
+# Stores the new password.
+arg="echo " + current_ip[:-1] + " > ~/stored_ip"
+p=subprocess.Popen(arg,shell=True,stdout=subprocess.PIPE)
